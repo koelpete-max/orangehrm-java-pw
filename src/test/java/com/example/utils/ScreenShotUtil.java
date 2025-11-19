2bacaa89c8d4fc14a08732307e7fe9bc2f22d056
koelpete-max <koelpete@gmail.com>
2025-11-19 21:45:56 +0100

    Fixing screenshot utils

--- src/test/java/com/example/utils/ScreenShotUtil.java
+++ src/test/java/com/example/utils/ScreenShotUtil.java
@@ -9,17 +9,16 @@ import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-// Screenshot-Util
 public class ScreenShotUtil {
 
     public static String takeScreenShot(Page page, String name) {
         var timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
         String fileName = name.replaceAll("[^a-zA-Z0-9-_]", "_") + timeStamp + ".jpg";
-        Path target = Paths.get("test-results", "screenshots", fileName);
+        Path target = Paths.get("test-output", "screenshots", fileName);
         try {
             Files.createDirectories(target.getParent());
             page.screenshot(new Page.ScreenshotOptions().setPath(target));
-            return target.toString(); // z.B. "test-results/screenshots/LoginTest.png"
+            return target.toString();
         } catch (IOException e) {
             throw new RuntimeException("Could not create screenshot", e);
         }
