dfa036b434683f1270bc92a7fb1a40164052a6b0
koelpete-max <koelpete@gmail.com>
2025-11-22 11:16:26 +0100

    CI - testing yml final images

--- src/test/java/com/example/base/BaseTest.java
+++ src/test/java/com/example/base/BaseTest.java
@@ -23,6 +23,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 
 import java.io.IOException;
 import java.io.InputStream;
