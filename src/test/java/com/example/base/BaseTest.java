61f2d2fe3c560391d8305036c68e0a61b9985196
koelpete-max <koelpete@gmail.com>
2025-11-22 14:34:30 +0100

    Including initial Setup Wizard

--- src/test/java/com/example/base/BaseTest.java
+++ src/test/java/com/example/base/BaseTest.java
@@ -19,6 +19,8 @@ import com.example.utils.TestConfig;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.microsoft.playwright.*;
+import com.microsoft.playwright.options.AriaRole;
+import com.microsoft.playwright.options.LoadState;
 import lombok.extern.slf4j.Slf4j;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
@@ -56,6 +58,127 @@ public class BaseTest {
 
     protected TestUser defaultTestUser;
 
+    @BeforeSuite
+    public void beforeSuite() {
+        playwright = Playwright.create();
+        browser = playwright.chromium().launch(
+                new BrowserType.LaunchOptions()
+                        .setArgs(Arrays.asList("--start-maximized"))
+                        .setHeadless(false)
+        );
+        page = browser.newPage();
+
+        baseUrl = System.getenv("BASE_URL");
+        baseUrl = (baseUrl == null || baseUrl.isBlank()) ? TestConfig.get("BASE_URL") : baseUrl;
+        if (baseUrl == null || baseUrl.isBlank()) {
+            throw new IllegalStateException("BASE_URL is not set");
+        }
+
+        page.navigate(baseUrl);
+        if (page.getByRole(AriaRole.HEADING).textContent().contains("Welcome to OrangeHRM Starter")) {
+            log.info("Setup Wizard is there!!");
+            goThroughSetupWizard();
+        }
+
+        browser.close();
+    }
+
+    private void goThroughSetupWizard() {
+
+        // Welcome to OrangeHRM Starter
+        log.info("==> Welcome to OrangeHRM Starter");
+        page.locator("label")
+                .filter(new Locator.FilterOptions()
+                        .setHasText("Upgrading an Existing"))
+                .locator("span").click();
+        page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions()
+                .setName("Next")).click();
+        page.locator("i.oxd-icon.bi-check.oxd-checkbox-input-icon")
+                .click();  // NEED Continue Button
+        page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions()
+                .setName("Continue")).click();
+        page.waitForLoadState(LoadState.DOMCONTENTLOADED);
+        page.waitForLoadState(LoadState.NETWORKIDLE);
+        page.waitForCondition(() -> {
+            return page.getByRole(AriaRole.TEXTBOX).first().isVisible();
+        });
+        if (!page.getByRole(AriaRole.HEADING).textContent().contains("Database Information".trim())) {
+            throw new IllegalStateException("Welcome to OrangeHRM Starter page failed");
+        }
+
+        // ENCRYPTION
+        log.info("==> ENCRYPTION");
+        page.getByRole(AriaRole.TEXTBOX).first().fill("orangehrm-db");
+        page.getByRole(AriaRole.TEXTBOX).nth(2).fill("orangehrm");
+        page.getByRole(AriaRole.TEXTBOX).nth(3).fill("orangeuser");
+        page.locator("input[type=\"password\"]").fill("orangepass");
+        page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Next")).click();
+        page.waitForCondition(() -> {
+            var success = page.getByRole(AriaRole.HEADING).textContent().contains("System Check".trim());
+            if (success) {
+                log.info("==> System Check heading is ready");
+            }
+            return success;
+        });
+        if (!page.getByRole(AriaRole.HEADING).textContent().contains("System Check".trim())) {
+            throw new IllegalStateException("ENCRYPTION page failed");
+        }
+
+        // System Check
+        log.info("==> System Check");
+        waitForPageCondition(page, "Next", "==> Button next page System is ready");
+        page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Next")).click();
+        if (!page.getByRole(AriaRole.HEADING).textContent().contains("Current Version Details")) {
+            throw new IllegalStateException("System Check page failed");
+        }
+
+        // Current Version Details
+        log.info("==> Current Version Details");
+        waitForPageCondition(page, "5.7", "==> Current OrangeHRM Version selection is ready");
+        if (!page.getByText("5.7", new Page.GetByTextOptions().setExact(true)).isVisible()) {
+            throw new IllegalStateException("Current Version Details page failed - Version 5.7 not found");
+        }
+        page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Next")).click();
+
+        waitForPageCondition(page, "Upgrading OrangeHRM", "==> Upgrading OrangeHRM is ready");
+        if (!page.getByText("Upgrading OrangeHRM").isVisible()) {
+            throw new IllegalStateException("Current Version Details page failed");
+        }
+
+        // Upgrading OrangeHRM
+        log.info("==> Upgrading OrangeHRM");
+        waitForPageCondition(page, "Next", "==> 100% bar ready");
+        page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Next")).click();
+        if (!page.getByRole(AriaRole.HEADING).textContent().contains("Upgrade Complete")) {
+            throw new IllegalStateException("Upgrading OrangeHRM page failed");
+        }
+
+        // Upgrade Complete
+        log.info("==> Upgrade Complete");
+        waitForPageCondition(page, "Launch OrangeHRM", "==> Launch OrangeHRM button is ready");
+        page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions()
+                .setName("Launch OrangeHRM"))
+                .click();
+        page.waitForCondition(() -> {
+            return page.locator("//img[@alt='company-branding']").isVisible();
+        });
+        if (!page.locator("//img[@alt='company-branding']").isVisible()) {
+            throw new IllegalStateException("Upgrade Complete page failed");
+        }
+
+        log.info("OrangeHRM page is ready!!");
+    }
+
+    private void waitForPageCondition(Page page, String text, String message) {
+        page.waitForCondition(() -> {
+            var visible = page.getByText(text, new Page.GetByTextOptions().setExact(true)).isVisible();
+            if (visible) {
+                log.info(message);
+            }
+            return visible;
+        });
+    }
+
     @BeforeMethod
     public  void setUp(Method method) throws IOException {
 
@@ -63,7 +186,7 @@ public class BaseTest {
         browser = playwright.chromium().launch(
                 new BrowserType.LaunchOptions()
                         .setArgs(Arrays.asList("--start-maximized"))
-                        .setHeadless(true)
+                        .setHeadless(false)
         );
         page = browser.newPage();
 
